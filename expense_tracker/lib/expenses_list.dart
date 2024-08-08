c25beb0664bdf69c47173c8638878945553ed883
joseAlvaradoD <josedejesus.alvarado.dominguez@gmail.com>
2024-08-08 17:57:55 -0600

    adding new expenses application

---
+++ expense_tracker/lib/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  
+  final List<Expense> expenses;
+
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
